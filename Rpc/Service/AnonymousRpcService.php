7e87a14d82b7ff9abe4a7c2c253a5625278b45f8
David Maignan <davidm@nationalfibre.net>
2014-06-02 13:38:35 +0000

    ARCH-628 implements anonymous rpc service and executable interface for LeadService

---
+++ Rpc/Service/AnonymousRpcService.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @copyright 2014 Instaclick Inc.
+ */
+
+namespace IC\Bundle\Base\RpcBundle\Rpc\Service;
+
+use IC\Bundle\Base\RpcBundle\Service\ExecutableInterface;
+use IC\Bundle\Base\RpcBundle\Service\RpcServiceInterface;
+use IC\Bundle\Base\RpcBundle\Service\RpcServiceModelInterface;
+
+/**
+ * Anonymous Rpc service
+ *
+ * @author David Maignan <davidm@nationalfibre.net>
+ */
+class AnonymousRpcService implements RpcServiceInterface, RpcServiceModelInterface
+{
+    /**
+     * @var object
+     */
+    private $model;
+
+    /**
+     * @var \IC\Bundle\Base\RpcBundle\Service\ExecutableInterface
+     */
+    private $service;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Define the model.
+     *
+     * @param string $model
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Define the executable Service.
+     *
+     * @param \IC\Bundle\Base\RpcBundle\Service\ExecutableInterface $service
+     */
+    public function setService(ExecutableInterface $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Execute leadService
+     *
+     * @param object $model
+     *
+     * @return string
+     */
+    public function execute($model)
+    {
+        return $this->service->execute($model);
+    }
+}
